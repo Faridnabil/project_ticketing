7bf0dc90a36a5e6b3983354b113260f35be22c8e
Farid Nabil Firdaus <faridnf121@gmail.com>
2024-06-22 17:02:57 +0700

    [1.16] Update Project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,34 +1,57 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import '@/plugins/lodash';
+import '@/plugins/moment';
+import '@/plugins/ladda';
+import '@/plugins/axios';
+import "@/components";
 
-require('./bootstrap');
+import Vue from "vue";
+import Vuex from 'vuex';
+import Meta from 'vue-meta';
+import SvgVue from 'svg-vue';
+import Notifications from 'vue-notification';
+import {VueReCaptcha} from "vue-recaptcha-v3";
+import VueElementLoading from 'vue-element-loading';
+import TextareaAutosize from 'vue-textarea-autosize';
+import vueFilterPrettyBytes from 'vue-filter-pretty-bytes';
 
-window.Vue = require('vue');
+import store from '@/store';
+import App from "@/views/app";
+import i18n from "@/language";
+import router from "@/views/router";
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+Vue.use(Vuex);
+Vue.use(Meta);
+Vue.use(SvgVue);
+Vue.use(Notifications);
+Vue.use(TextareaAutosize);
+Vue.use(vueFilterPrettyBytes);
+Vue.component('VueElementLoading', VueElementLoading);
+if (window.app.recaptcha_enabled) {
+    Vue.use(VueReCaptcha, {siteKey: window.app.recaptcha_public});
+}
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+Vue.config.productionTip = false;
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
-});
-
-
+new Vue({
+    i18n,
+    store,
+    router,
+    render: h => h(App),
+    mounted() {
+        this.initI18n();
+        this.$store.commit('setUser');
+        this.$store.commit('setSettings', window.app);
+    },
+    methods: {
+        initI18n() {
+            this.$i18n.locale = document.documentElement.lang;
+            this.loadTranslations();
+        },
+        loadTranslations() {
+            let self = this;
+            axios.get('api/lang/' + self.$i18n.locale).then(function (response) {
+                self.$i18n.setLocaleMessage(self.$i18n.locale, response.data);
+            });
+        },
+    }
+}).$mount("#app");
