675135238602f4f87226a685a7c1733713a27fc6
Algi M <109721495+AlgiM8@users.noreply.github.com>
2024-07-22 12:33:07 +0700

    [1.66] Update Active Sub menu

--- resources/views/layouts/dashboard/partials/sidebar.blade.php
+++ resources/views/layouts/dashboard/partials/sidebar.blade.php
@@ -1,3 +1,83 @@
+<style>
+    .menu-accordion.menu-active > .menu-link,
+    .menu-link.active {
+        background-color: #000;
+        color: #000;
+        width: 90%;
+        border-radius: 5px;
+        padding: 0.5rem;
+        margin-left: 10px;
+    }
+
+    .menu-sub {
+        background-color: #000;
+    }
+
+    .menu-sub.show {
+        display: block; /* Pastikan submenu terlihat */
+    }
+
+    .menu-link {
+        background-color: transparent;
+        color: black;
+    }
+
+    .menu-link.active {
+        background-color: #000;
+        /* Mengatur latar belakang merah untuk link aktif */
+        color: #000;
+        /* Mengubah warna teks agar kontras */
+        width: 90%;
+        /* Lebar 90% */
+        border-radius: 5px;
+        /* Border radius 5px */
+        padding: 0.5rem;
+        /* Padding opsional untuk jarak di dalam */
+        margin-left: 10px;
+    }
+</style>
+
+<style>
+    /* Menyediakan style untuk latar belakang dan teks pada item aktif */
+    .bg-red-500 {
+        background-color: #7ACFFF;
+        /* Merah */
+        border-radius: 5px;
+        margin-left: 10px;
+        padding: 0.5rem;
+        width: 90%;
+    }
+
+    .text-white {
+        color: #000;
+        /* Putih */
+    }
+
+    .bg-transparent {
+        background-color: transparent;
+        /* Transparan untuk item tidak aktif */
+    }
+
+    .text-black {
+        color: #000000;
+        /* Hitam untuk teks */
+    }
+</style>
+
+<style>
+    /* Style untuk teks aktif */
+    span.active {
+        color: rgb(255, 0, 0);
+        /* Warna teks hitam untuk item aktif */
+    }
+
+    /* Style untuk teks tidak aktif */
+    span.inactive {
+        color: #000;
+        /* Warna teks biru untuk item tidak aktif */
+    }
+</style>
+
 <div class="aside-menu flex-column-fluid">
     <!--begin::Aside Menu-->
     <div class="hover-scroll-overlay-y my-5 my-lg-5" id="kt_aside_menu_wrapper" data-kt-scroll="true"
@@ -119,7 +199,8 @@
 
             @can('View Province')
                 <div class="menu-item">
-                    <a class="menu-link {{ Request::is('province') ? 'active' : '' }}" href="{{ route('province.index') }}">
+                    <a class="menu-link {{ request()->routeIs('province.index') ? 'active' : '' }}"
+                        href="{{ route('province.index') }}">
                         <span class="menu-icon">
                             <!--begin::Svg Icon | path: icons/duotone/Interface/Doughnut.svg-->
                             <span class="svg-icon svg-icon-2">
@@ -142,7 +223,7 @@
 
             @can('View City Or Regency')
                 <div class="menu-item">
-                    <a class="menu-link {{ Request::is('cityOrRegency') ? 'active' : '' }}"
+                    <a class="menu-link {{ request()->routeIs('cityOrRegency.index') ? 'active' : '' }}"
                         href="{{ route('cityOrRegency.index') }}">
                         <span class="menu-icon">
                             <!--begin::Svg Icon | path: icons/duotone/Interface/Doughnut.svg-->
@@ -165,61 +246,67 @@
             @endcan
 
             @can('View User Management')
-                <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
-                    <span class="menu-link">
-                        <span class="menu-icon">
-                            <!--begin::Svg Icon | path: icons/duotone/General/User.svg-->
-                            <span class="svg-icon svg-icon-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                    width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                        <polygon points="0 0 24 0 24 24 0 24" />
-                                        <path
-                                            d="M12,11 C9.790861,11 8,9.209139 8,7 C8,4.790861 9.790861,3 12,3 C14.209139,3 16,4.790861 16,7 C16,9.209139 14.209139,11 12,11 Z"
-                                            fill="#000000" fill-rule="nonzero" opacity="0.3" />
-                                        <path
-                                            d="M3.00065168,20.1992055 C3.38825852,15.4265159 7.26191235,13 11.9833413,13 C16.7712164,13 20.7048837,15.2931929 20.9979143,20.2 C21.0095879,20.3954741 20.9979143,21 20.2466999,21 C16.541124,21 11.0347247,21 3.72750223,21 C3.47671215,21 2.97953825,20.45918 3.00065168,20.1992055 Z"
-                                            fill="#000000" fill-rule="nonzero" />
-                                    </g>
-                                </svg>
-                            </span>
-                            <!--end::Svg Icon-->
+            @php
+                // Cek apakah salah satu submenu aktif
+                $userManagementActive = request()->routeIs('user.index') || request()->routeIs('role.index') || request()->routeIs('permission.index');
+            @endphp
+            <div data-kt-menu-trigger="click" class="menu-item menu-accordion {{ $userManagementActive ? 'menu-active show' : '' }}">
+                <span class="menu-link">
+                    <span class="menu-icon">
+                        <!--begin::Svg Icon | path: icons/duotone/General/User.svg-->
+                        <span class="svg-icon svg-icon-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <polygon points="0 0 24 0 24 24 0 24" />
+                                    <path
+                                        d="M12,11 C9.790861,11 8,9.209139 8,7 C8,4.790861 9.790861,3 12,3 C14.209139,3 16,4.790861 16,7 C16,9.209139,14.209139,11 12,11 Z"
+                                        fill="#000000" fill-rule="nonzero" opacity="0.3" />
+                                    <path
+                                        d="M3.00065168,20.1992055 C3.38825852,15.4265159 7.26191235,13 11.9833413,13 C16.7712164,13 20.7048837,15.2931929 20.9979143,20.2 C21.0095879,20.3954741 20.9979143,21 20.2466999,21 C16.541124,21 11.0347247,21 3.72750223,21 C3.47671215,21 2.97953825,20.45918 3.00065168,20.1992055 Z"
+                                        fill="#000000" fill-rule="nonzero" />
+                                </g>
+                            </svg>
                         </span>
-                        <span class="menu-title">Manajemen Pengguna</span>
-                        <span class="menu-arrow"></span>
+                        <!--end::Svg Icon-->
                     </span>
-                    <div class="menu-sub menu-sub-accordion menu-active-bg">
-                        <div class="menu-item">
-                            <a class="menu-link" href="{{ route('user.index') }}">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Pengguna</span>
-                            </a>
-                        </div>
-                        <div class="menu-item">
-                            <a class="menu-link" href="{{ route('role.index') }}">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Peran</span>
-                            </a>
-                        </div>
-                        <div class="menu-item">
-                            <a class="menu-link" href="{{ route('permission.index') }}">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Ijin Akses</span>
-                            </a>
-                        </div>
+                    <span class="menu-title">Manajemen Pengguna</span>
+                    <span class="menu-arrow"></span>
+                </span>
+                <div class="menu-sub menu-sub-accordion menu-active-bg {{ $userManagementActive ? 'show' : '' }}">
+                    <div class="menu-item">
+                        <a class="menu-link mt-3 {{ request()->routeIs('user.index') ? 'active' : '' }}" href="{{ route('user.index') }}">
+                            <span class="menu-bullet">
+                                <span class="bullet bullet-dot"></span>
+                            </span>
+                            <span class="menu-title">Pengguna</span>
+                        </a>
+                    </div>
+                    <div class="menu-item">
+                        <a class="menu-link {{ request()->routeIs('role.index') ? 'active' : '' }}" href="{{ route('role.index') }}">
+                            <span class="menu-bullet">
+                                <span class="bullet bullet-dot"></span>
+                            </span>
+                            <span class="menu-title">Peran</span>
+                        </a>
+                    </div>
+                    <div class="menu-item">
+                        <a class="menu-link mb-3 {{ request()->routeIs('permission.index') ? 'active' : '' }}" href="{{ route('permission.index') }}">
+                            <span class="menu-bullet">
+                                <span class="bullet bullet-dot"></span>
+                            </span>
+                            <span class="menu-title">Ijin Akses</span>
+                        </a>
                     </div>
                 </div>
-            @endcan
+            </div>
+        @endcan
+
+
 
             @can('View Category')
                 <div class="menu-item">
-                    <a class="menu-link {{ Request::is('category') ? 'active' : '' }}"
+                    <a class="menu-link {{ request()->routeIs('category.index') ? 'active' : '' }}"
                         href="{{ route('category.index') }}">
                         <span class="menu-icon">
                             <!--begin::Svg Icon | path: icons/duotone/Design/PenAndRuller.svg-->
@@ -242,7 +329,7 @@
             @endcan
             @can('View Priority')
                 <div class="menu-item">
-                    <a class="menu-link {{ Request::is('priority') ? 'active' : '' }}"
+                    <a class="{{ request()->routeIs('priority.index') ? 'menu-link active' : 'menu-link' }}"
                         href="{{ route('priority.index') }}">
                         <span class="menu-icon">
                             <!--begin::Svg Icon | path: icons/duotone/Design/PenAndRuller.svg-->
@@ -263,9 +350,11 @@
                     </a>
                 </div>
             @endcan
+
             @can('View Status')
                 <div class="menu-item">
-                    <a class="menu-link {{ Request::is('status') ? 'active' : '' }}" href="{{ route('status.index') }}">
+                    <a class="{{ request()->routeIs('status.index') ? 'menu-link active' : 'menu-link' }}"
+                        href="{{ route('status.index') }}">
                         <span class="menu-icon">
                             <!--begin::Svg Icon | path: icons/duotone/Interface/Doughnut.svg-->
                             <span class="svg-icon svg-icon-2">
@@ -275,7 +364,7 @@
                                         d="M11 4.25769C11 3.07501 9.9663 2.13515 8.84397 2.50814C4.86766 3.82961 2 7.57987 2 11.9999C2 13.6101 2.38057 15.1314 3.05667 16.4788C3.58731 17.5363 4.98303 17.6028 5.81966 16.7662L5.91302 16.6728C6.60358 15.9823 6.65613 14.9011 6.3341 13.9791C6.11766 13.3594 6 12.6934 6 11.9999C6 9.62064 7.38488 7.56483 9.39252 6.59458C10.2721 6.16952 11 5.36732 11 4.39046V4.25769ZM16.4787 20.9434C17.5362 20.4127 17.6027 19.017 16.7661 18.1804L16.6727 18.087C15.9822 17.3964 14.901 17.3439 13.979 17.6659C13.3594 17.8823 12.6934 17.9999 12 17.9999C11.3066 17.9999 10.6406 17.8823 10.021 17.6659C9.09899 17.3439 8.01784 17.3964 7.3273 18.087L7.23392 18.1804C6.39728 19.017 6.4638 20.4127 7.52133 20.9434C8.86866 21.6194 10.3899 21.9999 12 21.9999C13.6101 21.9999 15.1313 21.6194 16.4787 20.9434Z"
                                         fill="#12131A" />
                                     <path fill-rule="evenodd" clip-rule="evenodd"
-                                        d="M13 4.39046C13 5.36732 13.7279 6.16952 14.6075 6.59458C16.6151 7.56483 18 9.62064 18 11.9999C18 12.6934 17.8823 13.3594 17.6659 13.9791C17.3439 14.9011 17.3964 15.9823 18.087 16.6728L18.1803 16.7662C19.017 17.6028 20.4127 17.5363 20.9433 16.4788C21.6194 15.1314 22 13.6101 22 11.9999C22 7.57987 19.1323 3.82961 15.156 2.50814C14.0337 2.13515 13 3.07501 13 4.25769V4.39046Z"
+                                        d="M13 4.39046C13 5.36732 13.7279 6.16952 14.6075 6.59458C16.6151 7.56483 18 9.62064 18 11.9999C18 12.6934 17.8823 13.3594 17.6659 13.9791C17.3439 14.9011 17.3964 15.9823 18.087 16.6728C19.017 17.6028 20.4127 17.5363 20.9433 16.4788C21.6194 15.1314 22 13.6101 22 11.9999C22 7.57987 19.1323 3.82961 15.156 2.50814C14.0337 2.13515 13 3.07501 13 4.25769V4.39046Z"
                                         fill="#12131A" />
                                 </svg>
                             </span>
@@ -286,6 +375,7 @@
                 </div>
             @endcan
 
+
             @can('View Ticket')
                 <div class="menu-item">
                     <div class="menu-content pt-8 pb-2">
@@ -295,7 +385,7 @@
                 @role('Admin')
                     @can('View Attendance')
                         <div class="menu-item">
-                            <a class="menu-link {{ Request::is('attendance') ? 'active' : '' }}"
+                            <a class="{{ request()->routeIs('attendance.index') ? 'menu-link active' : 'menu-link' }}"
                                 href="{{ route('attendance.index') }}">
                                 <span class="menu-icon">
                                     <!--begin::Svg Icon | path: icons/duotone/Design/PenAndRuller.svg-->
@@ -318,8 +408,10 @@
                     @endcan
 
 
+
                     <div class="menu-item">
-                        <a class="menu-link {{ Request::is('ticket') ? 'active' : '' }}" href="{{ route('ticket.index') }}">
+                        <a class="{{ request()->routeIs('ticket.index') ? 'menu-link active' : 'menu-link' }}"
+                            href="{{ route('ticket.index') }}">
                             <span class="menu-icon">
                                 <!--begin::Svg Icon | path: icons/duotone/Design/PenAndRuller.svg-->
                                 <span class="svg-icon svg-icon-2">
@@ -339,8 +431,9 @@
                         </a>
                     </div>
 
+
                     <div class="menu-item">
-                        <a class="menu-link {{ Request::is('requestAssignment') ? 'active' : '' }}"
+                        <a class="{{ request()->routeIs('requestAssignment.index') ? 'menu-link active' : 'menu-link' }}"
                             href="{{ route('requestAssignment.index') }}">
                             <span class="menu-icon">
                                 <!--begin::Svg Icon | path: icons/duotone/Design/PenAndRuller.svg-->
