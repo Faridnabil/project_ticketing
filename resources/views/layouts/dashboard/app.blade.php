581b236990f19e6d43d02df43697de04626f317b
Fakih <ilhamfakih50@gmail.com>
2025-01-07 16:36:28 +0700

    [1.0]update: login image

--- resources/views/layouts/dashboard/app.blade.php
+++ resources/views/layouts/dashboard/app.blade.php
@@ -124,8 +124,8 @@
                 <div class="aside-logo flex-column-auto" id="kt_aside_logo" style="background-color: white">
                     <!--begin::Logo-->
                     <a href="">
-                        <img alt="Logo" src="{{ asset('template/dist/assets/media/logos/logo.svg') }}"
-                            class="h-30px logo" />
+                        <img alt="Logo" src="{{ asset('template/dist/assets/media/logos/logo.png') }}"
+                            class="h-70px logo" />
                     </a>
                     <!--end::Logo-->
 
